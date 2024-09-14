32869b0a2f08f516ab0f24af4b52c75cd632758e
xiter22222 <pawanpediredla@gmail.com>
2024-09-14 19:08:53 +0530

    add : components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header className="py-6">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold">
+          Guillermo Rauch
+        </Link>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/about" className="hover:text-gray-300">
+                About
+              </Link>
+            </li>
+            <li>
+              <a href="https://twitter.com/your_twitter" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300">
+                Follow me
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
